2ed9416b1075c590363cbcf249cda098789ea44b
Rafael <grafa74@gmail.com>
2022-10-15 17:32:58 +0200

    added modules for board and logic

---
+++ Modules/cellsModifying.js
@@ -0,0 +1,10 @@
+import { gameOfLife } from './board.js';
+
+//import { AliveItems, countingAliveNeightbours } from './Modules/initialGrid.js';
+//import { DeathItems, countingDeathNeightbours } from './Modules/initialGrid.js';
+
+export const changingCellStatus = (board) => {
+    let board2 = board;
+    return board2;
+};
+console.log(changingCellStatus(gameOfLife));
